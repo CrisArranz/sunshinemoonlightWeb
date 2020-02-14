55dc4a21c845bd73e6ecf4009ac7a6cecb08a9c5
Cristian Arranz Arevalo <carranz@pclxit18.esmaddomw2k.selftrade.es>
2020-02-14 09:54:09 +0100

    SML - Commit: changes in modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TeddiesComponent } from './products/teddies/teddies.component';
-import { SheetsComponent } from './products/sheets/sheets.component';
-import { BadgesComponent } from './products/badges/badges.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeddiesComponent,
-    SheetsComponent,
-    BadgesComponent,
     ProductsComponent,
     FooterComponent
   ],
