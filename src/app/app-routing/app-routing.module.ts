951e173721702b56bff86129d293f1b9245791e5
Cristian Arranz Arevalo <carranz@pclxit18.esmaddomw2k.selftrade.es>
2020-02-21 09:52:38 +0100

    SML - Commit: Hotfix correct path in routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ProductsComponent } from '../products/products/products.component';
-import { ProductDetailComponent } from '../product-detail/product-detail/product-detail.component';
+import { ProductsComponent } from '../components/products/products/products.component';
+import { ProductDetailComponent } from '../components/product-detail/product-detail/product-detail.component';
 
 const appRoutes: Routes = [
     {path: 'products', component: ProductsComponent, pathMatch: 'prefix'},
